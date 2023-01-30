1b66f37f2a90941da714e35a116823509b6677c9
jeff <jeff@ezgovph.com>
2023-01-30 16:30:10 +0800

    edit post_cookie

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -43,7 +43,13 @@ class User extends BaseController
 			$data = $UserModel->getUserByEmail($_POST['username']);
 			$UserloginModel = new UserloginModel();
 			$UserloginData = $UserloginModel->find($data[0]['ExternalId']);
-			$post_data = json_decode($_POST['post_cookie']);
+			if(isset($_POST['post_cookie'])){
+				$post_data = json_decode($_POST['post_cookie']);	
+			}
+			else{
+				$post_data = array();
+			}
+			// $post_data = json_decode($_POST['post_cookie']);
 			$data_fields = ['IsSuccess'	=> 1,
 							  'IsMobile'	=> $this->request->getVar('device') == 0 ? 0:1,
 							  'IsWebsite'	=> $this->request->getVar('device') == 1 ? 0:1,
